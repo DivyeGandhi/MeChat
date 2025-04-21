bcbb2d749414b0ec426f269e5693f783187eea2b
DivyeGandhi <divye725@gmail.com>
2025-04-21 13:30:21 +0530

    Update routing to use react-router-dom v6 syntax

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ChatPage from './pages/ChatPage';
 import { ThemeProvider } from './context/ThemeContext';
 import ThemeSwitcher from './components/ThemeSwitcher';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ChatProvider from './context/ChatProvider';
+import { ChatProvider } from './context/ChatProvider';
 
 function App() {
   return (
-    <ChatProvider>
-      <Router>
+    <Router>
+      <ChatProvider>
         <ThemeProvider>
           <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-            <Switch>
-              <Route path="/" exact>
-                <HomePage />
-                <ThemeSwitcher />
-              </Route>
-              <Route path="/chats" component={ChatPage} />
-            </Switch>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/chats" element={<ChatPage />} />
+            </Routes>
+            <ThemeSwitcher />
             <ToastContainer 
               position="top-right"
               autoClose={3000}
@@ -35,8 +33,8 @@ function App() {
             />
           </div>
         </ThemeProvider>
-      </Router>
-    </ChatProvider>
+      </ChatProvider>
+    </Router>
   );
 }
 
